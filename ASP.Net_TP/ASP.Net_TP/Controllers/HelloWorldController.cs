03d5487b2d10426b452ab1e819eee218fe3a358b
DAUNAY <cldaunay@gmail.com>
2019-03-26 12:03:23 +0100

    Razor View Added

---
+++ ASP.Net_TP/ASP.Net_TP/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_TP.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name,int numTimes = 1)
+        {
+            /*return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");*/
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
